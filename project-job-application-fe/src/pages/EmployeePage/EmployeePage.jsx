9848948790dbba7893cfb4f98a1e3d7fade8059a
minhtuandev65 <huynhminhtuan13032003@gmail.com>
2025-07-14 15:29:19 +0700

    fix

--- project-job-application-fe/src/pages/EmployeePage/EmployeePage.jsx
+++ project-job-application-fe/src/pages/EmployeePage/EmployeePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getListPostAction } from "../../redux/actions/CompanyActions/CompanyActions";
-import HomePage from "./home/homePage";
+import HomePage from "./home/HomePage";
 
 function EmployeePage() {
   const dispatch = useDispatch();
